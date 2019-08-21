a31241d7b3dfeee59ceb47c2a95ce56dfb24165c
essjay05 <stepjoyous@gmail.com>
2019-08-20 18:13:56 -0700

    Modified create user control for efficiency.

--- controllers/users.js
+++ controllers/users.js
@@ -3,25 +3,14 @@ const User = require('../models/User');
 
 // Make controls exportable
 module.exports = {
-    // Create User
-    create: async ( req, res ) => {
-        console.log(req.body);
-    
-        let user = new User ({ 
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            username: req.body.username,
-            password: req.body.password
-        });
-    
-        try {
-            const savedUser = await user.save();
-            res.status(200).send(savedUser);
-        } catch (err) {
-            res.status(404).send(err);
-            console.log(err);
-        }
+    // Create User: WORKS
+    create: ( req, res ) => {
+        User.create(req.body, ( err, newUser ) => {
+            if (err) res.json({ success: false, payload: null, code: err.code });
+            // If user successfully created... give them a token:
+                // const token = signToken(newUser);
+            res.json({ success: true, newUser })
+        })
     }
     // Index User
 
